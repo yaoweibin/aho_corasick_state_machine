b4da542f99c6c27153d886d300f5cb49194c1b55
Weibin Yao <yaoweibin@gmail.com>
2011-05-27 09:37:36 +0000

    add goto function

--- acsm.c
+++ acsm.c
@@ -18,6 +18,85 @@
 * */
 
 
+typedef struct acsm_queue_s {
+    struct acsm_queue_s  *prev;
+    struct acsm_queue_s  *next;
+} acsm_queue_t;
+
+typedef struct {
+    int          state;
+    acsm_queue_t queue;
+} acsm_state_queue_t;
+
+
+#define acsm_queue_init(q)                                                    \
+    (q)->prev = q;                                                            \
+    (q)->next = q
+
+
+#define acsm_queue_empty(h)                                                   \
+    (h == (h)->prev)
+
+
+#define acsm_queue_insert_head(h, x)                                          \
+    (x)->next = (h)->next;                                                    \
+    (x)->next->prev = x;                                                      \
+    (x)->prev = h;                                                            \
+    (h)->next = x
+
+
+#define acsm_queue_insert_tail(h, x)                                          \
+    (x)->prev = (h)->prev;                                                    \
+    (x)->prev->next = x;                                                      \
+    (x)->next = h;                                                            \
+    (h)->prev = x
+
+
+#define acsm_queue_head(h)                                                    \
+    (h)->next
+
+
+#define acsm_queue_last(h)                                                    \
+    (h)->prev
+
+
+#define acsm_queue_sentinel(h)                                                \
+    (h)
+
+
+#define acsm_queue_next(q)                                                    \
+    (q)->next
+
+
+#define acsm_queue_prev(q)                                                    \
+    (q)->prev
+
+
+#define acsm_queue_remove(x)                                                  \
+    (x)->next->prev = (x)->prev;                                              \
+    (x)->prev->next = (x)->next
+
+
+#define acsm_queue_split(h, q, n)                                             \
+    (n)->prev = (h)->prev;                                                    \
+    (n)->prev->next = n;                                                      \
+    (n)->next = q;                                                            \
+    (h)->prev = (q)->prev;                                                    \
+    (h)->prev->next = h;                                                      \
+    (q)->prev = n;
+
+
+#define acsm_queue_add(h, n)                                                  \
+    (h)->prev->next = (n)->next;                                              \
+    (n)->next->prev = (h)->prev;                                              \
+    (h)->prev = (n)->prev;                                                    \
+    (h)->prev->next = h;
+
+
+#define acsm_queue_data(q, type, link)                                        \
+    (type *) ((u_char *) q - offsetof(type, link))
+
+
 acsm_context_t *acsm_alloc(int flag)
 {
     int no_case = 0;
@@ -34,6 +113,7 @@ acsm_context_t *acsm_alloc(int flag)
 
     ctx->no_case = no_case;
     ctx->max_state = 1;
+    ctx->num_state = 0;
 
     return ctx;
 }
@@ -81,23 +161,76 @@ int acsm_add_pattern(acsm_context_t *ctx, u_char *string)
 
 int acsm_complie(acsm_context_t *ctx)
 {
-    unsigned int i, j;
+    int state, new_state;
+    u_char ch;
+    unsigned int i, j, k;
+    acsm_pattern_t *p, *copy;
 
     ctx->state_table = malloc(ctx->max_state * sizeof(acsm_state_node_t));
     if (ctx->state_table == NULL) {
         return -1;
     }
 
-    for (i = 0; i < ctx->max_state; i++) {
+    for (i = 1; i < ctx->max_state; i++) {
 
         ctx->state_table[i].fail_state = 0;
+        ctx->state_table[i].match_list = NULL;
 
         for (j = 0; j < ASCIITABLE_SIZE; j++) {
             ctx->state_table[i].next_state[j] = ACSM_FAIL_STATE;
         }
     }
 
-    /*TODO*/
+    for (j = 0; j < ASCIITABLE_SIZE; j++) {
+        ctx->state_table[0].next_state[j] = ACSM_FAIL_STATE;
+    }
+
+    /* Construction of the goto function */
+    p = ctx->patterns;
+    while (p) {
+        state = 0;
+        j = 0;
+
+        while(j < p->len) {
+
+            ch = p->string[j];
+            if (ctx->state_table[state].next_state[ch] == ACSM_FAIL_STATE) {
+                break;
+            }
+
+            state = ctx->state_table[state].next_state[ch];
+            j++;
+        }
+
+        for (k = j; k < p->len; k++) {
+            new_state = ++ctx->num_state;
+            ch = p->string[k];
+
+            ctx->state_table[state].next_state[ch] = new_state;
+            state = new_state;
+        }
+
+        copy = malloc(sizeof(acsm_pattern_t));
+        if (copy == NULL) {
+            return -1;
+        }
+        memcpy(copy, p, sizeof(acsm_pattern_t));
+
+        copy->next = ctx->state_table[state].match_list; 
+        ctx->state_table[state].match_list = copy; 
+
+        p = p->next;
+    }
+
+    /* Construction of the failure function */
+    acsm_queue_t        *qu, queue;
+    acsm_state_queue_t  free_queue;
+
+    qu = &queue;
+    acsm_queue_init(qu);
+    qu = &free_queue.queue;
+    acsm_queue_init(qu);
+
 
     return 0;
 }
@@ -114,7 +247,7 @@ char *input =
 
 int main() 
 {
-    u_char *input;
+    u_char **input;
     acsm_context_t *ctx;
 
     ctx = acsm_alloc(0);
@@ -123,13 +256,12 @@ int main()
         return -1;
     }
 
-    input = (u_char *) test_patterns;
+    input = (u_char**) test_patterns;
    
-    while(input) {
-        acsm_add_pattern(ctx, input);
+    while(*input) {
+        acsm_add_pattern(ctx, *input);
         input++;
     }
-
 
     acsm_free(ctx);
 
